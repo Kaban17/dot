d9fe532e831917a38a1470cc7b7fc97352f35f3e
Никита Кондартьев <107360354+Kaban17@users.noreply.github.com>
2025-05-16 11:26:37 +0400

    Delete nvim directory

--- nvim/lua/plugins/treesitter.lua
+++
@@ -1,8 +0,0 @@
-require 'nvim-treesitter.configs'.setup{
-	ensure_installed = {"lua", "go", "rust", "python", "asm", "bash", "bibtex", "c", "cpp", "cmake", "css", "html","dockerfile", "markdown", "todotxt"}, 
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-	},
-}
